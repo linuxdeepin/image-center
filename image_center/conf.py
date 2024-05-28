688088e26d83285a3986ae671aec277857f659a6
CCrazyPeter <1520389703@qq.com>
2024-05-28 20:32:12 +0800

    update

--- image_center/conf.py
+++ image_center/conf.py
@@ -4,11 +4,36 @@
 # SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 
 # SPDX-License-Identifier: Apache Software License
+import enum
 import os
 import platform
 import tempfile
 
+@enum.unique
+class DisplayServer(enum.Enum):
+    wayland = "wayland"
+    x11 = "x11"
+
+@enum.unique
+class PlatForm(enum.Enum):
+    win = "Windows"
+    linux = "Linux"
+    macos = "Darwin"
+
 class _Setting:
+    """配置模块"""
+
+    SERVER_IP = "127.0.0.1"
+    PORT = 8889
+    NETWORK_RETRY = 1
+    PAUSE = 1
+    TIMEOUT = 5
+    MAX_MATCH_NUMBER = 100
+
+    IS_LINUX = False
+    IS_WINDOWS = False
+    IS_MACOS = False
+
     PIC_PATH = ""
     IMAGE_RATE = 0.9
     # Win default path——C:\\Users\\xxxx\\AppData\\Local\\Temp
@@ -18,33 +43,22 @@ class _Setting:
     # IMAGE_MATCH_NUMBER = 1
     # IMAGE_MATCH_WAIT_TIME = 1
 
-    # RPC config
-    SERVER_IP = ""
-    PORT = 8889
-    NETWORK_RETRY =1
-    PAUSE = 1
-    TIMEOUT = 5
-    MAX_MATCH_NUMBER = 100
-    IS_X11 = False
-    IS_WAYLAND = False
-
-    if platform.system() == "Linux":
-        # 显示服务器
-        DISPLAY_SERVER = os.popen(
-            "cat ~/.xsession-errors | grep XDG_SESSION_TYPE | head -n 1"
-        ).read().split("=")[-1].strip("\n")
-
-        class DisplayServer:
-            wayland = "wayland"
-            x11 = "x11"
-
-        IS_X11 = (DISPLAY_SERVER == DisplayServer.x11)
-        IS_WAYLAND = (DISPLAY_SERVER == DisplayServer.wayland)
-    elif platform.system() == "Darwin":
-        # MacOS
-        IS_MACOS = True
-    elif platform.system() == "Windows":
+    if platform.system() == PlatForm.win.value:
         # windows
         IS_WINDOWS = True
+    elif platform.system() == PlatForm.macos.value:
+        # MacOS
+        IS_MACOS = True
+    elif platform.system() == PlatForm.linux.value:
+        # 显示服务器
+        DISPLAY_SERVER = (
+            os.popen("cat ~/.xsession-errors | grep XDG_SESSION_TYPE | head -n 1")
+            .read()
+            .split("=")[-1]
+            .strip("\n")
+        )
+
+        IS_X11 = DISPLAY_SERVER == DisplayServer.x11.value
+        IS_WAYLAND = DISPLAY_SERVER == DisplayServer.wayland.value
 
 setting = _Setting()
